52d617398fe714059dcd7dce016b77e0de779477
AkhilaChinta <chintaakhila123@gmail.com>
2024-04-21 04:32:16 -0400

    Vue commit

---
+++ src/components/TaskHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>Task Scheduler</h1>
+    </header>
+</template>
+
+<script>
+    export default{
+        name:"TaskHeader"
+    }
+</script>
+
+<style scoped>
+    header{
+        text-align:center;
+    }
+</style>
